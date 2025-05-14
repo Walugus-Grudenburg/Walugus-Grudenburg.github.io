9ec68a1d7fabd98eca3d63d81317b10cc19314cc
Walugus-Grudenburg <superstinkyfarts@gmail.com>
2025-05-13 22:20:51 -0400

    1.0

--- script.js
+++ script.js
@@ -1,4 +1,158 @@
-zalgoGenerator = import('./zalgo-generator')
+const zalgoUpArray = [
+    '\u030d',
+    '\u030e',
+    '\u0304',
+    '\u0305',
+    '\u033f',
+    '\u0311',
+    '\u0306',
+    '\u0310',
+    '\u0352',
+    '\u0357',
+    '\u0351',
+    '\u0307',
+    '\u0308',
+    '\u030a',
+    '\u0342',
+    '\u0343',
+    '\u0344',
+    '\u034a',
+    '\u034b',
+    '\u034c',
+    '\u0303',
+    '\u0302',
+    '\u030c',
+    '\u0350',
+    '\u0300',
+    '\u0301',
+    '\u030b',
+    '\u030f',
+    '\u0312',
+    '\u0313',
+    '\u0314',
+    '\u033d',
+    '\u0309',
+    '\u0363',
+    '\u0364',
+    '\u0365',
+    '\u0366',
+    '\u0367',
+    '\u0368',
+    '\u0369',
+    '\u036a',
+    '\u036b',
+    '\u036c',
+    '\u036d',
+    '\u036e',
+    '\u036f',
+    '\u033e',
+    '\u035b',
+    '\u0346',
+    '\u031a', /*     ̚    */
+];
+const zalgoDownArray = [
+    '\u0316',
+    '\u0317',
+    '\u0318',
+    '\u0319',
+    '\u031c',
+    '\u031d',
+    '\u031e',
+    '\u031f',
+    '\u0320',
+    '\u0324',
+    '\u0325',
+    '\u0326',
+    '\u0329',
+    '\u032a',
+    '\u032b',
+    '\u032c',
+    '\u032d',
+    '\u032e',
+    '\u032f',
+    '\u0330',
+    '\u0331',
+    '\u0332',
+    '\u0333',
+    '\u0339',
+    '\u033a',
+    '\u033b',
+    '\u033c',
+    '\u0345',
+    '\u0347',
+    '\u0348',
+    '\u0349',
+    '\u034d',
+    '\u034e',
+    '\u0353',
+    '\u0354',
+    '\u0355',
+    '\u0356',
+    '\u0359',
+    '\u035a',
+    '\u0323', /*     ̣     */
+];
+const zalgoMidArray = [
+    '\u0315',
+    '\u031b',
+    '\u0340',
+    '\u0341',
+    '\u0358',
+    '\u0321',
+    '\u0322',
+    '\u0327',
+    '\u0328',
+    '\u0334',
+    '\u0335',
+    '\u0336',
+    '\u034f',
+    '\u035c',
+    '\u035d',
+    '\u035e',
+    '\u035f',
+    '\u0360',
+    '\u0362',
+    '\u0338',
+    '\u0337',
+    '\u0361',
+    '\u0489',
+];
+
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+
+const addZalgoToChar = (character, zalgoCount, zalgoArray) => {
+    for (let i = 0; i < zalgoCount; i++) {
+        character += zalgoArray[getRandomInt(zalgoArray.length)];
+    }
+    return character;
+};
+const addZalgoToString = (text, zalgoCount, zalgoArray) => {
+    let result = '';
+    for (const char of text) {
+        result += addZalgoToChar(char, zalgoCount, zalgoArray);
+    }
+    return result;
+};
+const zalgoGeneration = (text, zalgoUpCount, zalgoMidCount, zalgoDownCount) => {
+    text = addZalgoToString(text, zalgoUpCount, zalgoUpArray);
+    text = addZalgoToString(text, zalgoMidCount, zalgoMidArray);
+    return addZalgoToString(text, zalgoDownCount, zalgoDownArray);
+};
+const zalgoRandomGeneration = (text, zalgoCount) => zalgoGeneration(text, getRandomInt(zalgoCount), getRandomInt(zalgoCount), getRandomInt(zalgoCount));
+
+const unzalgoString = (textZalgo, zalgoArray) => {
+    for (const zalgoChar of zalgoArray) {
+        textZalgo = textZalgo.split(zalgoChar).join('');
+    }
+    return textZalgo;
+};
+const unzalgoText = (textZalgo) => {
+    textZalgo = unzalgoString(textZalgo, zalgoUpArray);
+    textZalgo = unzalgoString(textZalgo, zalgoMidArray);
+    textZalgo = unzalgoString(textZalgo, zalgoDownArray);
+    return textZalgo;
+};
+
 const morseCodeMap = {
   'A': '.-',     'B': '-...',   'C': '-.-.',
   'D': '-..',    'E': '.',      'F': '..-.',
@@ -42,6 +196,30 @@ const wingdingsMap = {
   '~': '❞︎', ' ': ' '
 }
 
+const flippedTextMap = {
+  'a': 'ɐ', 'b': 'q', 'c': 'ɔ',
+  'd': 'p', 'e': 'ǝ', 'f': 'ɟ',
+  'g': 'ƃ', 'h': 'ɥ', 'i': 'ᴉ',
+  'j': 'ɾ', 'k': 'ʞ', 'l': 'l',
+  'm': 'ɯ', 'n': 'u', 'o': 'o',
+  'p': 'd', 'q': 'b', 'r': 'ɹ',
+  's': 's', 't': 'ʇ', 'u': 'n',
+  'v': 'ʌ', 'w': 'ʍ', 'x': 'x',
+  'y': 'ʎ', 'z': 'z',
+  'A': '∀', 'B': 'q', 'C': 'Ɔ',
+  'D': 'p', 'E': 'Ǝ', 'F': 'Ⅎ',
+  'G': 'פ', 'H': 'H', 'I': 'I',
+  'J': 'ſ', 'K': 'ʞ', 'L': '˥',
+  'M': 'W', 'N': 'N', 'O': 'O',
+  'P': 'Ԁ', 'Q': 'Q', 'R': 'ɹ',
+  'S': 'S', 'T': '┴', 'U': '∩',
+  'V': 'Λ', 'W': 'M', 'X': 'X',
+  'Y': '⅄', 'Z': 'Z',
+  ',': '\'', '.': '˙', '?': '¿', 
+  '!': '¡', '\'': ',', '\"': ',,',
+  '~': '~', ' ': ' '
+}
+
 function stripVariationSelectors(str) {
   return str.replace(/[\uFE0E\uFE0F]/g, '');
 }
@@ -52,6 +230,10 @@ const reverseMorseCodeMap = Object.fromEntries(
 
 const reverseWingDingsMap = Object.fromEntries(
   Object.entries(wingdingsMap).map(([k, v]) => [stripVariationSelectors(v), k])
+);
+
+const reverseFlippedTextMap = Object.fromEntries(
+  Object.entries(flippedTextMap).map(([k, v]) => [stripVariationSelectors(v), k])
 );
 
 let mode = "Al3x/Morse Code"
@@ -98,6 +280,9 @@ function toNormal(){
     case "Viling/Zalgo Text":
       fromZalgo();
       break; 
+    case "Unjust/Flipped Text":
+      fromFT()
+      break;
     default:
       break;
   }
@@ -128,6 +313,9 @@ function toGimmick(){
       break;
     case "Viling/Zalgo Text":
       toZalgo();
+      break;
+    case "Unjust/Flipped Text":
+      toFT()
       break;  
     default:
       break;
@@ -281,11 +469,19 @@ function toRAL(){
 }
 
 function fromZalgo(){
-  normalText.value = 0;
+  normalText.value = unzalgoText(gimmickText.value);
 }
 
 function toZalgo(){
-  gimmickText.value = zalgoGenerator.zalgoGeneration(normalText);
+  gimmickText.value = zalgoGeneration(normalText.value,1,1,1);
+}
+
+function toFT() {
+  gimmickText.value = normalText.value.split('').map(char => flippedTextMap[char] || '').join('');
+}
+
+function fromFT() {
+  normalText.value = [...gimmickText.value].map(char => reverseFlippedTextMap[stripVariationSelectors(char)] || '').join('');
 }
 
 normalText.addEventListener("keyup", toGimmick)
